fd0c79718c9597a88452382b061f2e4f0ca5493b
Cat21 <meowsionw@gmail.com>
2023-09-08 10:56:20 +0200

    Updated index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body class="container">
+
+<header>
+<marquee>"Is that a Cat21 reference?" - K--swiss</marquee>
+</header>
+
+<main>
+	<p>Hiiiii!!!!! ⸜(｡˃ ᵕ ˂ )⸝</p>
+	<br>
+	<p>My name is Cat21 and I am an amateur artist, programmer and video editor!!!
+	<br>
+	<p>In my free time, I like drawing, watching anime and playing video gamez!!! (*♡∀♡)
+	<br>
+	<p>I have 2 cats, Emurka and Rišo (they are both very good goobers)</p>
+
+	<section>
+	<section id="left">
+		<img src="assets/IMG_20210128_091522.jpg" alt="Emurka">
+		<figcaption><p>Emurka <br> Why is she doing the 😯 face tho! 🤣🤣🤣🤣🤣</p></figcaption>
+	</section>
+	<section id="right">
+		<img src="assets/IMG_20210130_103437.jpg" alt="Rišo">
+		<figcaption><p>Rišo <br> He is kinda wonky 😵‍😵‍😵‍😵</p></figcaption>
+	</section>
+	
+</main>
+
+<br>
+<footer>
+	<p>Copyright 2023 trolololololol</p>
+</footer>
